e8b3c6d9530b166a78e01d574bf1b4edd8981ea6
shiiiima <shimaazimi75@gmail.com>
2020-10-04 03:38:40 +0330

    version 1

---
+++ dist/js/renderBlog.js
@@ -0,0 +1,34 @@
+
+async  function loadBlogJson(){
+	let result={};
+try{
+	result = await $.get('https://reyton.net/wp-json/wp/v2/cpt_1457?_embed');
+	console.log(result);
+	return result ;
+
+}catch(e){
+console.error('API is Down or bad requested!')
+}
+} 
+async function renderblog(){
+	let posts = await loadBlogJson();
+	let html = '';
+	posts.forEach(post => {
+		
+		let temp = `<div class="col-md-3 col-6 postblog text-secondary p-2 ">
+		<div class="card bordershadow recordCard" >
+		<img class="card-img-top " src="${post._embedded['wp:featuredmedia'][0].source_url}">
+		<a href="${post.link}">
+		<div class="card-body">
+		 <p class="card-title text-secondary h6 ">${post.title.rendered}</p>
+		 <span class="badge badge-pill badge-success"> comments :${post.id}
+		</div>
+		</a>
+	  </div>
+	  </div>`
+	
+		html+=temp
+	});
+	$('#postsPart').append(html);
+} 
+renderblog();
